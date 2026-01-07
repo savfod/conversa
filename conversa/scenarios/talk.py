6eb90a2751334678b4486d940e4cff7e3d4e5b6a
Alex Savchik <savsmail@gmail.com>
2026-01-08 00:01:00 +0100

    scenarios/answer -> features/answer

--- conversa/scenarios/talk.py
+++ conversa/scenarios/talk.py
@@ -8,8 +8,8 @@ from conversa.audio.audio_parser import AudioParser
 from conversa.audio.input_stream.base import AbstractAudioInputStream
 from conversa.audio.output_stream.base import AbstractAudioOutputStream
 from conversa.audio.speech_api import speech_to_text, text_to_speech
+from conversa.features.answer import teacher_answer
 from conversa.features.find_errors import check_for_errors
-from conversa.scenarios.answer import teacher_answer
 from conversa.util.io import (
     DEFAULT_AUDIO_DIR,
     DEFAULT_CONVERSATIONS_FILE,
